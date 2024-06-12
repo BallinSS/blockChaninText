b96fecbfc8915b528bb41505aa4e87e1af450992
Ballinss <877423800@qq.com>
2024-06-12 11:15:40 +0800

    遍历区块信息与增加区块到区块链里

--- BLC/blockChain.go
+++ BLC/blockChain.go
@@ -1,7 +1,9 @@
 package BLC
 
 import (
+	"fmt"
 	"log"
+	"math/big"
 
 	"github.com/boltdb/bolt"
 )
@@ -24,6 +26,79 @@ const dbName = "blockchain.db"
 // 表的名字
 const blockTableName = "blocks"
 
+// 遍历区块信息：
+// 1.验证区块链的完整性和一致性：
+// 通过遍历区块链，可以检查每个区块的前一个区块的哈希是否匹配，以确保区块链没有被篡改。
+
+// 2.调试和监控：
+// 在开发和测试区块链应用程序时，遍历区块链可以帮助你了解区块链的状态、调试问题和监控链上的数据。
+
+// 3.展示和分析数据：
+// 遍历区块链可以用来展示链上的交易数据和其他信息，帮助用户理解区块链的内容。
+
+// 4.数据备份和恢复：
+// 在一些情况下，遍历区块链可以用于数据备份和恢复操作，确保链上的数据可以被完整地恢复。
+func (blc *BlockChain) PrintChain() {
+	var block *Block
+	var currentHash []byte = blc.Tip
+	for {
+		err := blc.DB.View(func(tx *bolt.Tx) error {
+			b := tx.Bucket([]byte(blockTableName))
+			if b != nil {
+				blockBytes := b.Get(currentHash)
+				block = DeserializeBlock(blockBytes)
+				fmt.Printf("Height:%d\n", block.Height)
+				fmt.Printf("PrevBlockHash:%x\n", block.PrevBlockHash)
+				fmt.Printf("Data:%s\n", block.Data)
+				fmt.Printf("Timestamp:%d\n", block.Timestamp)
+				fmt.Printf("Hash:%x\n", block.Hash)
+				fmt.Printf("Nonce:%d\n", block.Nonce)
+			}
+			return nil
+		})
+		fmt.Println()
+		if err != nil {
+			log.Panic(err)
+		}
+		var hashInt big.Int
+		hashInt.SetBytes(block.PrevBlockHash)
+		if big.NewInt(0).Cmp(&hashInt) == 0 {
+			break
+		}
+		currentHash = block.PrevBlockHash
+	}
+}
+
+// 增加区块到区块链里
+func (blc *BlockChain) AddBlockToBlockchain(data string) {
+	// Update(fn func(*bolt.Tx) error) error
+	err := blc.DB.Update(func(tx *bolt.Tx) error {
+		// 获取区块链数据
+		b := tx.Bucket([]byte(blockTableName))
+		//创建新区块
+		if b != nil {
+			//获取最新区块
+			blockBytes := b.Get(blc.Tip)
+			block := DeserializeBlock(blockBytes)
+			//将区块序列化并存储到数据中
+			newBlock := NewBlock(block.Height+1, block.Hash, data)
+			err := b.Put(newBlock.Hash, newBlock.Serialize())
+			if err != nil {
+				log.Panic(err)
+			}
+			err = b.Put([]byte("1"), newBlock.Hash)
+			if err != nil {
+				log.Panic(err)
+			}
+			blc.Tip = newBlock.Hash
+		}
+		return nil
+	})
+	if err != nil {
+		log.Panic(err)
+	}
+}
+
 // 创世区块创建
 func CreatBlockWithGenesisBlock() *BlockChain {
 
@@ -32,7 +107,6 @@ func CreatBlockWithGenesisBlock() *BlockChain {
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer db.Close()
 	var blockHash []byte
 	err = db.Update(func(tx *bolt.Tx) error {
 		b, err := tx.CreateBucket([]byte(blockTableName))
--- main.go
+++ main.go
@@ -2,45 +2,11 @@ package main
 
 import (
 	"blc/BLC"
-	"fmt"
-	"log"
-
-	"github.com/boltdb/bolt"
 )
 
 func main() {
-	//创建打开数据库
-	db, err := bolt.Open("my.db", 0600, nil)
-	if err != nil {
-		//立即停止
-		log.Fatal(err)
-	}
-	defer db.Close()
+	blockChain := BLC.CreatBlockWithGenesisBlock()
+	defer blockChain.DB.Close()
 
-	err = db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte("blocks"))
-		if b != nil {
-			blockData := b.Get([]byte("l"))
-			block := BLC.DeserializeBlock(blockData)
-			fmt.Printf("%v\n", block)
-		}
-		return nil
-	})
-
-	if err != nil {
-		// 执行defer后停止
-		log.Panic(err)
-	}
-	// 初始化区块链
-	// block := BLC.NewBlock(1, []byte{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}, "Test")
-	// fmt.Printf("%d\n", block.Nonce)
-	// fmt.Printf("%x\n", block.Hash)
-	// bytes := block.Serialize()
-
-	// fmt.Println(bytes)
-
-	// block = BLC.DeserializeBlock(bytes)
-
-	// fmt.Printf("%d\n", block.Nonce)
-	// fmt.Printf("%x\n", block.Hash)
+	blockChain.AddBlockToBlockchain("send 100BTC to Ballin")
 }
